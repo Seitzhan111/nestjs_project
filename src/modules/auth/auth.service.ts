a45cf69a4df19987b59513e9ea1adeacdb8442d5
Seitzhan <sbukebaev@gmail.com>
2023-11-13 09:51:20 +0600

    Refactoring app - Improved auth service - Improved watchlist service - Improved user service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AppError } from '../../common/constants/error';
 import { UserLoginDto } from './dto/user-login.dto';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 import { AuthUserResponse } from './response';
 import { TokenService } from '../token/token.service';
 
@@ -11,22 +11,33 @@ import { TokenService } from '../token/token.service';
 export class AuthService {
   constructor(
     private readonly userService: UsersService,
-    private readonly tokenService: TokenService
+    private readonly tokenService: TokenService,
   ) {}
 
-  async registerUsers (dto: CreateUserDto): Promise<CreateUserDto> {
-    const existUser = await this.userService.findUserByEmail(dto.email)
-    if (existUser) throw new BadRequestException(AppError.USER_EXIST)
-    return this.userService.createUser(dto)
+  async registerUsers(dto: CreateUserDto): Promise<CreateUserDto> {
+    try {
+      const existUser = await this.userService.findUserByEmail(dto.email);
+      if (existUser) throw new BadRequestException(AppError.USER_EXIST);
+      return this.userService.createUser(dto);
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 
-  async loginUser (dto: UserLoginDto): Promise<AuthUserResponse> {
-    const existUser = await this.userService.findUserByEmail(dto.email)
-    if (!existUser) throw new BadRequestException(AppError.USER_NOT_EXIST)
-    const validatePassword = await bcrypt.compare(dto.password, existUser.password)
-    if (!validatePassword) throw new BadRequestException(AppError.WRONG_DATA)
-    const user = await this.userService.publicUser(dto.email)
-    const token = await this.tokenService.generateJwtToken(user)
-    return {user, token}
+  async loginUser(dto: UserLoginDto): Promise<AuthUserResponse> {
+    try {
+      const existUser = await this.userService.findUserByEmail(dto.email);
+      if (!existUser) throw new BadRequestException(AppError.USER_NOT_EXIST);
+      const validatePassword = await bcrypt.compare(
+        dto.password,
+        existUser.password,
+      );
+      if (!validatePassword) throw new BadRequestException(AppError.WRONG_DATA);
+      const user = await this.userService.publicUser(dto.email);
+      const token = await this.tokenService.generateJwtToken(user);
+      return { user, token };
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 }
